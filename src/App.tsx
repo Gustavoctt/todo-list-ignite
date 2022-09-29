d274ef7811df5fbc179cb5fb3b2ec77bf9d770e8
Gustavoctt <gustavocarrertartare@gmail.com>
2022-09-29 18:38:47 -0300

    feat: Create tasks layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Header } from './components/Header/Header';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
 import { BsClipboardX } from 'react-icons/bs';
+import { HiOutlineTrash } from 'react-icons/hi';
 
 import styles from './App.module.css';
 import './global.css';
+import { useState } from 'react';
 
 
 export function App() {
+  const [tasks, setTasks] = useState([])
   return (
    <>
     <Header />
@@ -36,11 +39,35 @@ export function App() {
           </div>
         </div>
 
-        <div className={styles.tasksEmpty}>
-          <BsClipboardX size={56}/>
-          <strong>Você ainda não tem tarefas cadastradas</strong>
-          <p>Crie tarefas e organize seus itens a fazer</p>
-        </div>
+       
+       { tasks.length > 0 ? (
+          <div className={styles.tasksEmpty}>
+            <BsClipboardX size={56}/>
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        ) : (
+          <div className={styles.tasksFull}>
+            <div className={styles.taskItem}>
+              <div className={styles.inputCheckbox}>
+                <input type="checkbox"  id='checkbox'/>
+              </div>
+              
+              <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+              <HiOutlineTrash size={24} />
+            </div> 
+
+            <div className={styles.taskItem}>
+              <div className={styles.inputCheckbox}>
+                <input type="checkbox"  id='checkbox'/>
+              </div>
+              <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+              <HiOutlineTrash size={24}/>
+            </div> 
+          </div>
+        ) 
+       }
+
       </section>
     </main>
    </>
